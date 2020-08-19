f28e04716ed988f94ffcf10d20eedaa393cf5735
Ajay Kumar Reddy <kajaykumar20@gmail.com>
2020-08-19 10:41:17 +0530

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
+
+import { Home, Rooms, Error, SingleRoom } from './pages';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 function App() {
-  return <>hello from app</>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <div>hello from Home page</div>;
+}
+
+export default Home;
